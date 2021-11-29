c0e525d299d2e968bfb86ab4097075b5ea317225
Meghana Balarama <meghana1704@gmail.com>
2021-11-28 21:34:02 -0500

    Delete DenseFlow directory

--- DenseFlow/denseflow/nn/blocks/__init__.py
+++
@@ -1,6 +0,0 @@
-from .denseblock import DenseLayer, DenseBlock, ResidualDenseBlock, MultiHeadDenseBlock
-
-from .autoregressive import *
-
-from .attention import MultiheadAttention
-from .resblock import ResidualBlock
--- DenseFlow/nn/nets/matching/__init__.py
+++
@@ -1,3 +0,0 @@
-from .densenet import DenseNet, DenseNetMultihead, PureDenseNet
-from .multiscale_densenet import MultiscaleDenseNet
-from .resnet import ResNet
